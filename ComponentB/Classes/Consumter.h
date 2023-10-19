4f2fe1240cc47e45a917c55b49fd76d4551fa579
pengchao <pengchao@dreame.tech>
2023-10-19 13:17:42 +0800

    增加新文件

---
+++ ComponentB/Classes/Consumter.h
@@ -0,0 +1,16 @@
+//
+//  Consumter.h
+//  test
+//
+//  Created by pengchao on 2023/10/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Consumter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ComponentB/Classes/Consumter.m
@@ -0,0 +1,12 @@
+//
+//  Consumter.m
+//  test
+//
+//  Created by pengchao on 2023/10/19.
+//
+
+#import "Consumter.h"
+
+@implementation Consumter
+
+@end
